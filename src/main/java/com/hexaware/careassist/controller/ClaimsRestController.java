ac53f6d06b7e9415b4bf9ee524f6c30ae9e3bf4f
Yash <yashdubey413@gmail.com>
2024-02-25 12:26:00 +0530

    minor changes

--- src/main/java/com/hexaware/careassist/controller/ClaimsRestController.java
+++ src/main/java/com/hexaware/careassist/controller/ClaimsRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ Description : Creation of ClaimsRestController
 
 @RestController
 @RequestMapping("/api/v1/claims")
+@CrossOrigin(origins="http://localhost:4200")
 public class ClaimsRestController {
 	
 	@Autowired
